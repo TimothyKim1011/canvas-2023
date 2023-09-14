774e5cb74b166c5eb9f2e15b45d1495b0c764c19
TimothyKim1011 <138592141+TimothyKim1011@users.noreply.github.com>
2023-09-14 17:51:13 +0900

    0914

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMouseMove(event) {
@@ -11,6 +12,7 @@ function onMouseMove(event) {
     ctx.stroke();
     return;
   }
+  ctx.beginPath();
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 function startPainting() {
@@ -19,7 +21,14 @@ function startPainting() {
 function cancelPainting() {
   isPainting = false;
 }
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
